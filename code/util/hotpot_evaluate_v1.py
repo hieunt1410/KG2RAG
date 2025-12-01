dfd8897efd16fc84a3a5782a3165ec182e81418f
hieungo1410 <hieungo1410@gmail.com>
2025-12-01 14:40:11 +0700

    update musique dataset

--- code/util/hotpot_evaluate_v1.py
+++ code/util/hotpot_evaluate_v1.py
@@ -3,19 +3,30 @@ import ujson as json
 import re
 import string
 from collections import Counter
-import pickle
+
+def normalize_entity_name(entity):
+    """
+    Remove disambiguators from entity names.
+    Examples:
+        "Ed Wood (film)" -> "Ed Wood"
+        "Deliver Us from Evil (2014 film)" -> "Deliver Us from Evil"
+        "Scott Derrickson" -> "Scott Derrickson" (unchanged)
+    """
+    # Remove parenthetical disambiguators at the end
+    entity = re.sub(r'\s*\([^)]*\)\s*$', '', entity)
+    return entity.strip()
 
 def normalize_answer(s):
 
     def remove_articles(text):
-        return re.sub(r'\b(a|an|the)\b', ' ', text)
+        return re.sub(r"\b(a|an|the)\b", " ", text)
 
     def white_space_fix(text):
-        return ' '.join(text.split())
+        return " ".join(text.split())
 
     def remove_punc(text):
         exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
+        return "".join(ch for ch in text if ch not in exclude)
 
     def lower(text):
         return text.lower()
@@ -29,9 +40,15 @@ def f1_score(prediction, ground_truth):
 
     ZERO_METRIC = (0, 0, 0)
 
-    if normalized_prediction in ['yes', 'no', 'noanswer'] and normalized_prediction != normalized_ground_truth:
+    if (
+        normalized_prediction in ["yes", "no", "noanswer"]
+        and normalized_prediction != normalized_ground_truth
+    ):
         return ZERO_METRIC
-    if normalized_ground_truth in ['yes', 'no', 'noanswer'] and normalized_prediction != normalized_ground_truth:
+    if (
+        normalized_ground_truth in ["yes", "no", "noanswer"]
+        and normalized_prediction != normalized_ground_truth
+    ):
         return ZERO_METRIC
 
     prediction_tokens = normalized_prediction.split()
@@ -47,20 +64,32 @@ def f1_score(prediction, ground_truth):
 
 
 def exact_match_score(prediction, ground_truth):
-    return (normalize_answer(prediction) == normalize_answer(ground_truth))
+    return normalize_answer(prediction) == normalize_answer(ground_truth)
+
 
 def update_answer(metrics, prediction, gold):
     em = exact_match_score(prediction, gold)
     f1, prec, recall = f1_score(prediction, gold)
-    metrics['em'] += float(em)
-    metrics['f1'] += f1
-    metrics['prec'] += prec
-    metrics['recall'] += recall
+    metrics["em"] += float(em)
+    metrics["f1"] += f1
+    metrics["prec"] += prec
+    metrics["recall"] += recall
     return em, prec, recall
 
+
 def update_sp(metrics, prediction, gold):
-    cur_sp_pred = set(map(tuple, prediction))
-    gold_sp_pred = set(map(tuple, gold))
+    # Normalize entity names in predictions
+    cur_sp_pred = set()
+    for ent, idx in prediction:
+        normalized_ent = normalize_entity_name(ent)
+        cur_sp_pred.add((normalized_ent, idx))
+    
+    # Normalize entity names in gold
+    gold_sp_pred = set()
+    for ent, idx in gold:
+        normalized_ent = normalize_entity_name(ent)
+        gold_sp_pred.add((normalized_ent, idx))
+    
     tp, fp, fn = 0, 0, 0
     for e in cur_sp_pred:
         if e in gold_sp_pred:
@@ -74,11 +103,12 @@ def update_sp(metrics, prediction, gold):
     recall = 1.0 * tp / (tp + fn) if tp + fn > 0 else 0.0
     f1 = 2 * prec * recall / (prec + recall) if prec + recall > 0 else 0.0
     em = 1.0 if fp + fn == 0 else 0.0
-    metrics['sp_em'] += em
-    metrics['sp_f1'] += f1
-    metrics['sp_prec'] += prec
-    metrics['sp_recall'] += recall
+    metrics["sp_em"] += em
+    metrics["sp_f1"] += f1
+    metrics["sp_prec"] += prec
+    metrics["sp_recall"] += recall
     return em, prec, recall
+
 
 def eval(prediction_file, gold_file):
     with open(prediction_file) as f:
@@ -86,26 +116,39 @@ def eval(prediction_file, gold_file):
     with open(gold_file) as f:
         gold = json.load(f)
 
-    metrics = {'em': 0, 'f1': 0, 'prec': 0, 'recall': 0,
-        'sp_em': 0, 'sp_f1': 0, 'sp_prec': 0, 'sp_recall': 0,
-        'joint_em': 0, 'joint_f1': 0, 'joint_prec': 0, 'joint_recall': 0}
+    metrics = {
+        "em": 0,
+        "f1": 0,
+        "prec": 0,
+        "recall": 0,
+        "sp_em": 0,
+        "sp_f1": 0,
+        "sp_prec": 0,
+        "sp_recall": 0,
+        "joint_em": 0,
+        "joint_f1": 0,
+        "joint_prec": 0,
+        "joint_recall": 0,
+    }
     count = 0
     for dp in gold:
-        cur_id = dp['_id']
+        cur_id = dp["_id"]
         can_eval_joint = True
-        if cur_id not in prediction['answer']:
+        if cur_id not in prediction["answer"]:
             # print('missing answer {}'.format(cur_id))
             can_eval_joint = False
             continue
         else:
             em, prec, recall = update_answer(
-                metrics, prediction['answer'][cur_id], dp['answer'])
-        if cur_id not in prediction['sp']:
-            print('missing sp fact {}'.format(cur_id))
+                metrics, prediction["answer"][cur_id], dp["answer"]
+            )
+        if cur_id not in prediction["sp"]:
+            print("missing sp fact {}".format(cur_id))
             can_eval_joint = False
         else:
             sp_em, sp_prec, sp_recall = update_sp(
-                metrics, prediction['sp'][cur_id], dp['supporting_facts'])
+                metrics, prediction["sp"][cur_id], dp["supporting_facts"]
+            )
         count += 1
 
         if can_eval_joint:
@@ -114,13 +157,13 @@ def eval(prediction_file, gold_file):
             if joint_prec + joint_recall > 0:
                 joint_f1 = 2 * joint_prec * joint_recall / (joint_prec + joint_recall)
             else:
-                joint_f1 = 0.
+                joint_f1 = 0.0
             joint_em = em * sp_em
 
-            metrics['joint_em'] += joint_em
-            metrics['joint_f1'] += joint_f1
-            metrics['joint_prec'] += joint_prec
-            metrics['joint_recall'] += joint_recall
+            metrics["joint_em"] += joint_em
+            metrics["joint_f1"] += joint_f1
+            metrics["joint_prec"] += joint_prec
+            metrics["joint_recall"] += joint_recall
 
     # N = len(gold)
     for k in metrics.keys():
@@ -129,6 +172,6 @@ def eval(prediction_file, gold_file):
 
     print(metrics)
 
-if __name__ == '__main__':
-    eval(sys.argv[1], sys.argv[2])
 
+if __name__ == "__main__":
+    eval(sys.argv[1], sys.argv[2])
